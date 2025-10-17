44502429ece8c5567e3c8bc5e8796c82d7440f7c
Vendetta1871 <conqueror.v.v.v.v.v@gmail.com>
2025-10-17 19:10:11 +0800

    Added natural light

--- src/main/java/zh/vendetta/heightmaplod/LODMeshBuilder.java
+++ src/main/java/zh/vendetta/heightmaplod/LODMeshBuilder.java
@@ -40,6 +40,22 @@ public class LODMeshBuilder {
         v1 = sprite.getMaxV();
     }
 
+    private static void getUv(IBlockState state) {
+        Minecraft mc = Minecraft.getMinecraft();
+        TextureMap map = mc.getTextureMapBlocks();
+
+        //IBlockState state = Blocks.WOOL.getDefaultState()
+                //.withProperty(BlockColored.COLOR, EnumDyeColor.WHITE);
+
+        TextureAtlasSprite sprite = mc.getBlockRendererDispatcher()
+                .getModelForState(state).getParticleTexture();
+
+        u0 = sprite.getMinU();
+        u1 = sprite.getMaxU();
+        v0 = sprite.getMinV();
+        v1 = sprite.getMaxV();
+    }
+
     private static int heightmap(World world, int x, int z, int i) {
         int height = 0;
         for (int ix = x; ix < x + i; ++ix) {
@@ -48,6 +64,17 @@ public class LODMeshBuilder {
             }
         }
         return height / i / i;
+    }
+
+    private static int brightness(World world, int x, int z, int i) {
+        int max = 0;
+        for (int ix = x; ix < x + i; ++ix) {
+            for (int iz = z; iz < z + i; ++iz) {
+                int b = world.getCombinedLight(new BlockPos(ix, world.getHeight(ix, iz), iz), 0);
+                if (b > max) max = b;
+            }
+        }
+        return max;
     }
 
     public static void buildLODMesh(World world, BlockPos chunkPos, BufferBuilder builder, int i) {
@@ -65,10 +92,13 @@ public class LODMeshBuilder {
             for (int z = 0; z < 16; z += i) {
                 int worldX = baseX + x;
                 int worldZ = baseZ + z;
-                float h = (float) heightmap(world, baseX + x, baseZ + z, i);
 
+                float h = (float) heightmap(world, worldX, worldZ, i);
                 if (h < chunkPos.getY() || h > chunkPos.getY() + 15) continue;
+
+                int b = brightness(world, worldX, worldZ, i);
                 //
+                //getUv(world.getBlockState(new BlockPos(worldX, h - 1, worldZ)).getBlock().getDefaultState());
 
                 // TOP FACE
 
@@ -79,18 +109,19 @@ public class LODMeshBuilder {
                         Float.floatToRawIntBits(0f), Float.floatToRawIntBits(0f), Float.floatToRawIntBits(0f), 0xFFFFFFFF, Float.floatToRawIntBits(u0), Float.floatToRawIntBits(v1), 0
                 };
 
+                //buffer.putBrightness4(b1, b2, b3, b4);
 
                 builder.addVertexData(vertexData);
-                builder.putBrightness4(240, 240, 240, 240); // свет
-                builder.putColorMultiplier(0.2f, 1.0f, 0.4f, 4); // множитель цвета
-                builder.putColorMultiplier(0.2f, 1.0f, 0.4f, 3);
-                builder.putColorMultiplier(0.2f, 1.0f, 0.4f, 2);
-                builder.putColorMultiplier(0.2f, 1.0f, 0.4f, 1);
+                builder.putBrightness4(b, b, b, b); // свет
+                //builder.putColorMultiplier(0.2f, 1.0f, 0.4f, 4); // множитель цвета
+                //builder.putColorMultiplier(0.2f, 1.0f, 0.4f, 3);
+                //builder.putColorMultiplier(0.2f, 1.0f, 0.4f, 2);
+                //builder.putColorMultiplier(0.2f, 1.0f, 0.4f, 1);
                 builder.putPosition(worldX, h, worldZ); // смещение
 
                 // BACK FACE
 
-                float dh1 = (float) (heightmap(world, baseX + x, baseZ + z, i) - heightmap(world, baseX + x, baseZ + z - i, i));
+                float dh1 = (float) (heightmap(world, worldX, worldZ, i) - heightmap(world, worldX, worldZ - i, i));
 
                 if (dh1 > 0) {
                     int[] vertexData1 = new int[]{
@@ -101,15 +132,15 @@ public class LODMeshBuilder {
                     };
 
                     builder.addVertexData(vertexData1);
-                    builder.putBrightness4(240, 240, 240, 240); // свет
-                    builder.putColorMultiplier(1.0f, 0.5f, 1.0f, 4); // множитель цвета
-                    builder.putColorMultiplier(1.0f, 0.5f, 1.0f, 3);
-                    builder.putColorMultiplier(1.0f, 0.5f, 1.0f, 2);
-                    builder.putColorMultiplier(1.0f, 0.5f, 1.0f, 1);
+                    builder.putBrightness4(b, b, b, b); // свет
+                    //builder.putColorMultiplier(1.0f, 0.5f, 1.0f, 4); // множитель цвета
+                    //builder.putColorMultiplier(1.0f, 0.5f, 1.0f, 3);
+                    //builder.putColorMultiplier(1.0f, 0.5f, 1.0f, 2);
+                    //builder.putColorMultiplier(1.0f, 0.5f, 1.0f, 1);
                     builder.putPosition(worldX, h, worldZ); // смещение
                 }
 
-                float dh2 = (float) (heightmap(world, baseX + x, baseZ + z, i) - heightmap(world, baseX + x, baseZ + z + i, i));
+                float dh2 = (float) (heightmap(world, worldX, worldZ, i) - heightmap(world, worldX, worldZ + i, i));
 
                 if (dh2 > 0) {
                     int[] vertexData1 = new int[]{
@@ -120,17 +151,17 @@ public class LODMeshBuilder {
                     };
 
                     builder.addVertexData(vertexData1);
-                    builder.putBrightness4(240, 240, 240, 240); // свет
-                    builder.putColorMultiplier(1.0f, 0.5f, 0.4f, 4); // множитель цвета
-                    builder.putColorMultiplier(1.0f, 0.5f, 0.4f, 3);
-                    builder.putColorMultiplier(1.0f, 0.5f, 0.4f, 2);
-                    builder.putColorMultiplier(1.0f, 0.5f, 0.4f, 1);
+                    builder.putBrightness4(b, b, b, b); // свет
+                    //builder.putColorMultiplier(1.0f, 0.5f, 0.4f, 4); // множитель цвета
+                    //builder.putColorMultiplier(1.0f, 0.5f, 0.4f, 3);
+                    //builder.putColorMultiplier(1.0f, 0.5f, 0.4f, 2);
+                    //builder.putColorMultiplier(1.0f, 0.5f, 0.4f, 1);
                     builder.putPosition(worldX, h, worldZ); // смещение
                 }
 
                 // LEFT FACE
 
-                float dh3 = (float) (heightmap(world, baseX + x, baseZ + z, i) - heightmap(world, baseX + x - i, baseZ + z, i));
+                float dh3 = (float) (heightmap(world, worldX, worldZ, i) - heightmap(world, worldX - i, worldZ, i));
 
                 if (dh3 > 0) {
                     int[] vertexData1 = new int[]{
@@ -141,17 +172,17 @@ public class LODMeshBuilder {
                     };
 
                     builder.addVertexData(vertexData1);
-                    builder.putBrightness4(240, 240, 240, 240); // свет
-                    builder.putColorMultiplier(.20f, 0.2f, 0.1f, 4); // множитель цвета
-                    builder.putColorMultiplier(.20f, 0.2f, 0.1f, 3);
-                    builder.putColorMultiplier(.20f, 0.2f, 0.1f, 2);
-                    builder.putColorMultiplier(.20f, 0.2f, 0.1f, 1);
+                    builder.putBrightness4(b, b, b, b); // свет
+                    //builder.putColorMultiplier(.20f, 0.2f, 0.1f, 4); // множитель цвета
+                    //builder.putColorMultiplier(.20f, 0.2f, 0.1f, 3);
+                    //builder.putColorMultiplier(.20f, 0.2f, 0.1f, 2);
+                    //builder.putColorMultiplier(.20f, 0.2f, 0.1f, 1);
                     builder.putPosition(worldX, h, worldZ); // смещение
                 }
 
                 // RIGHT FACE
 
-                float dh4 = (float) (heightmap(world, baseX + x, baseZ + z, i) - heightmap(world, baseX + x + i, baseZ + z, i));
+                float dh4 = (float) (heightmap(world, worldX, worldZ, i) - heightmap(world, worldX + i, worldZ, i));
 
                 if (dh4 > 0) {
                     int[] vertexData1 = new int[]{
@@ -162,11 +193,11 @@ public class LODMeshBuilder {
                     };
 
                     builder.addVertexData(vertexData1);
-                    builder.putBrightness4(240, 240, 240, 240); // свет
-                    builder.putColorMultiplier(.20f, 0.2f, 0.1f, 4); // множитель цвета
-                    builder.putColorMultiplier(.20f, 0.2f, 0.1f, 3);
-                    builder.putColorMultiplier(.20f, 0.2f, 0.1f, 2);
-                    builder.putColorMultiplier(.20f, 0.2f, 0.1f, 1);
+                    builder.putBrightness4(b, b, b, b); // свет
+                    //builder.putColorMultiplier(.20f, 0.2f, 0.1f, 4); // множитель цвета
+                    //builder.putColorMultiplier(.20f, 0.2f, 0.1f, 3);
+                    //builder.putColorMultiplier(.20f, 0.2f, 0.1f, 2);
+                    //builder.putColorMultiplier(.20f, 0.2f, 0.1f, 1);
                     builder.putPosition(worldX, h, worldZ); // смещение
                 }
             }
